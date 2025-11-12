5d60e80febf8d87f1053872bfd473ffb769d3edc
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2025-11-12 10:40:00 +0100

    v0.1

---
+++ Libro.java
@@ -0,0 +1,11 @@
+package libro;
+
+public class Libro {
+    String titulo;
+    String autor;
+    int anio;
+
+    public static void main(String[] args) {
+        Libro libro = new Libro();
+    }
+}
